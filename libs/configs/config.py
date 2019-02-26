23ac44485fd7b496faecf591c2030b0cf16b1117
Rui Chen <ruichen9618@gmail.com>
2019-02-26 17:08:44 +0800

    begin pascal voc dataloader

---
+++ libs/configs/config.py
@@ -0,0 +1,15 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import time
+
+"""RUN Configs"""
+cuda = True
+display = 10
+log_image = 200
+restore = None
+
+"""TRAINING"""
+start_over = False
+max_epoch = 30
---
+++ libs/datasets/pascal_voc.py
@@ -0,0 +1,26 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import torch
+import os
+import os.path as osp
+import PIL
+import cv2
+import numpy as np
+import scipy.sparse
+import scipy.io as sio
+import cPickle
+import uuid
+import xml.etree.ElementTree as ET
+import libs.configs.config as cfg
+
+from .dataloader import sDataLoader
+from libs.boxes.cython_bbox as cython_bbox
+from libs.nets.utils import everything2tensor
+from libs.layers.data_layer import data_layer_keep_aspect_ratio, \
+    data_layer_keep_aspect_ratio_batch
+
+def unique_boxes():
+    pass
+
