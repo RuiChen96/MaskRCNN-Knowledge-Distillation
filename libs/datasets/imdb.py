cb8e1602614e0b40f3cabfc9f895b2b288861654
Rui Chen <ruichen9618@gmail.com>
2019-02-27 11:38:46 +0800

    xx

--- libs/datasets/imdb.py
+++ libs/datasets/imdb.py
@@ -95,4 +95,55 @@ class imdb(object):
         num_images = self.num_images
         widths = self._get_widths()
         for i in xrange(num_images):
-            
+            boxes = self.roidb[i]['boxes'].copy()
+            oldx1 = boxes[:, 0].copy()
+            oldx2 = boxes[:, 2].copy()
+            boxes[:, 0] = widths[i] - oldx2 - 1
+            boxes[:, 2] = widths[i] - oldx1 - 1
+            assert (boxes[:, 2] >= boxes[:, 0]).all()
+            entry = {'boxes' : boxes,
+                     'gt_overlaps' : self.roidb[i]['gt_overlaps'],
+                     'gt_classes' : self.roidb[i]['gt_classes'],
+                     'flipped' : True}
+            self.roidb.append(entry)
+        # if dontcare_areas
+        # end_for
+        self._image_index = self._image_index * 2
+
+    def evaluate_recall(self, candidate_boxes=None, thresholds=None,
+                        area='all', limit=None):
+        """Evaluate detection proposal recall metrics.
+
+        Returns:
+            results: dictionary of results with keys
+                'ar': average recall
+                'recalls': vector recalls at each IoU overlap threshold
+                'thresholds': vector of IoU overlap thresholds
+                'gt_overlaps': vector of all ground-truth overlaps
+
+        :param candidate_boxes:
+        :param thresholds:
+        :param area:
+        :param limit:
+        :return:
+        """
+        # Record max overlap value for each gt box
+        # Return vector of overlap values
+        areas = {'all': 0, 'small': 1, 'medium': 2, 'large': 3,
+                 '96-128': 4, '128-256': 5, '256-512': 6, '512-inf': 7}
+        area_ranges = [[0**2, 1e5**2],      # all
+                       [0**2, 32**2],       # small
+                       [32**2, 96**2],      # medium
+                       [96**2, 1e5**2],     # large
+                       [96**2, 128**2],     # 96-128
+                       [128**2, 256**2],    # 128-256
+                       [256**2, 512**2],    # 256-512
+                       [512**2, 1e5**2],    # 512-inf
+                       ]
+        assert areas.has_key(area), 'unknown area range: {}'.format(area)
+        area_range = area_ranges[areas[area]]
+        gt_overlaps = np.zeros(0)
+        num_pos = 0
+        for i in xrange(self.num_images):
+            max_gt_overlaps = self.roidb[i]['gt_overlaps'].toarray().max(axis=1)
+            gt_inds = np.where()
