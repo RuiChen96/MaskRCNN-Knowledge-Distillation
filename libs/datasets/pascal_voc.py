8c6df74542dba1abfad5afd17ed918951468bbc8
Rui Chen <ruichen9618@gmail.com>
2019-03-01 11:50:31 +0800

    xx

--- libs/datasets/pascal_voc.py
+++ libs/datasets/pascal_voc.py
@@ -2,8 +2,12 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
-import torch
+# import torch
+
 import os
+from libs.datasets.imdb import imdb
+import libs.datasets.ds_utils as ds_utils
+
 import os.path as osp
 import PIL
 import cv2
@@ -21,6 +25,48 @@ from libs.nets.utils import everything2tensor
 from libs.layers.data_layer import data_layer_keep_aspect_ratio, \
     data_layer_keep_aspect_ratio_batch
 
+class pascal_voc(imdb):
+    def __init__(self, image_set, year, devkit_path=None, is_training=True):
+        imdb.__init__(self, 'voc_' + year + '_' + image_set)
+        self._year = year
+        self._image_set = image_set
+        self._devkit_path = self._get_default_path() if devkit_path is None \
+                            else devkit_path
+        self._data_path = os.path.join(self._devkit_path, 'VOC' + self._year)
+        self.__classes = ('__background__', # always index 0
+                          'aeroplane', 'bicycle', 'bird', 'boat',
+                          'bottle', 'bus', 'car', 'cat', 'chair',
+                          'cow', 'diningtable', 'dog', 'horse',
+                          'motorbike', 'person', 'pottedplant',
+                          'sheep', 'sofa', 'train', 'tvmonitor'
+                          )
+        self._class_to_ind = dict(zip(self.classes), xrange(self.num_classes))
+        self._image_ext = '.jpg'
+        self._image_index = self._load_image_set_index()
+        # Default to roidb handler
+        self._roidb_handler = self.selective_search_roidb
+        self._salt = str(uuid.uuid4())
+        self._comp_id = 'comp4'
+
+        # PASCAL specific config options
+        self.config = {'cleanup':       True,
+                       'use_salt':      True,
+                       'use_diff':      False,
+                       'matlab_eval':   False,
+                       'rpn_file':      None,
+                       'min_size':      2
+                       }
+        assert os.path.exists(self._devkit_path), \
+                'VOC devkit path does not exist: {}'.format(self._devkit_path)
+        assert os.path.exists(self._data_path), \
+                'Data path does not exist: {}'.format(self._data_path)
+
+        # for pytorch dataloader
+        self._gt_annotations = self.gt_roidb()
+        self._is_training = is_training
+
+    def image_path_at(self, i):
+        pass
 
 
 if __name__ == '__main__':
