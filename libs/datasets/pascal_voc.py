1cb81369d6ac2dd752984b7caf13a3d934bd2298
Rui Chen <ruichen9618@gmail.com>
2019-03-05 15:06:35 +0800

    xx

--- libs/datasets/pascal_voc.py
+++ libs/datasets/pascal_voc.py
@@ -7,7 +7,7 @@ from __future__ import print_function
 import os
 from libs.datasets.imdb import imdb
 import libs.datasets.ds_utils as ds_utils
-
+from libs.datasets.voc_eval import voc_eval
 import os.path as osp
 import PIL
 import cv2
@@ -284,9 +284,75 @@ class pascal_voc(imdb):
                         )
 
     def _do_python_eval(self, output_dir = 'output'):
+        annopath = os.path.join(
+            self._devkit_path,
+            'VOC' + self._year,
+            'Annotations',
+            '{:s}.xml'
+        )
+        imagesetfile = os.path.join(
+            self._devkit_path,
+            'VOC' + self._year,
+            'ImageSets',
+            'Main',
+            self._image_set + '.txt'
+        )
+        cachedir = os.path.join(self._devkit_path, 'annotations_cache')
+        aps = []
+        # The PASCAL VOC metric changed in 2010
+        use_07_metric = True if int(self._year) < 2010 else False
+        # use_07_metric = False
+        print('VOC07 metric? ' + ('Yes' if use_07_metric else 'No'))
+        if not os.path.isdir(output_dir):
+            os.mkdir(output_dir)
+        for i, cls in enumerate(self._classes):
+            if cls == '__background__':
+                continue
+            filename = self._get_voc_results_file_template().format(cls)
+            rec, prec, ap = voc_eval(
+                filename, annopath, imagesetfile, cls, cachedir, ovthresh=0.5,
+                use_07_metric=use_07_metric
+            )
+            aps += [ap]
+            print('AP for {} = {:.4f}, Recall {:.4f}'.format(cls, ap, rec.max()))
+            with open(os.path.join(output_dir, cls + '_pr.pkl'), 'w') as f:
+                cPickle.dump({'rec': rec, 'prec': prec, 'ap': ap}, f)
+        # end_for
+        # each cls has the same weight for the mean value
+        print('Mean AP = {:.4f}'.format(np.mean(aps)))
+        print('~~~~~~~~')
+        print('Results:')
+        for ap in aps:
+            print('{:.3f}'.format(ap))
+        print('{:.3f}'.format(np.mean(aps)))
+        print('~~~~~~~~')
+        print('')
+        print('--------------------------------------------------------------')
+        print('Results computed with the **unofficial** Python eval code.')
+        print('Results should be very close to the official MATLAB eval code.')
+        print('Recompute with `./tools/reval.py --matlab ...` for your paper.')
+        print('-- Thanks, The Management')
+        print('--------------------------------------------------------------')
+
+    def _do_matlab_eval(self, output_dir = 'output'):
+        print('-----------------------------------------------------')
+        print('Computing results with the official MATLAB eval code.')
+        print('-----------------------------------------------------')
+
+        # path = os.path.join(cfg.data_dir, 'lib', 'datasets',
+        #                     'VOCdevkit-matlab-wrapper')
+        # cmd = 'cd {} && '.format(path)
+        # cmd += '{:s} -nodisplay -nodesktop '.format(cfg.MATLAB)
+        # cmd += '-r "dbstop if error; '
+        # cmd += 'voc_eval(\'{:s}\',\'{:s}\',\'{:s}\',\'{:s}\'); quit;"' \
+        #        .format(self._devkit_path, self._get_comp_id(),
+        #                self._image_set, output_dir)
+        # print('Running:\n{}'.format(cmd))
+        # status = subprocess.call(cmd, shell=True)
         pass
 
-
+    def evaluate_detections(self, all_boxes, output_dir='output'):
+        pass
 
 
 if __name__ == '__main__':
